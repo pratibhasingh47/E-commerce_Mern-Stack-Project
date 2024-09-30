cb1ccce08d7f75eb6217fac07b3782293e22828c
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-09-30 21:45:19 +0530

    project_updated

--- backend/model/user.js
+++ backend/model/user.js
@@ -3,86 +3,220 @@ const validator = require("validator");
 const bcrypt = require("bcrypt");
 
 const userSchema = mongoose.Schema({
-    googleId : {
-        type : String,
-        default : null
+    googleId: {
+        type: String,
+        default: null
     },
-    name : {
-        type : String,
-        required : [true , "Name is required"],
-        minlength : [3 , "Name should be atleast 3 charater long"],
-        maxlength : [50, "Name cannot exceed 50 charaters"],
-        validate :  {
-            validator : function(value){
-                return validator.isAlpha(value , "en-US" , {ignore : " "});
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        minlength: [3, "Name should be at least 3 characters long"],
+        maxlength: [50, "Name cannot exceed 50 characters"],
+        validate: {
+            validator: function (value) {
+                return validator.isAlpha(value, "en-US", { ignore: " " });
             },
-            message : "Name should be in String"
+            message: "Name should be in string format"
         }
     },
-    email : {
-        type : String,
-        required :  [true , "Email is required"],
-        unique : true,
-        validate :  {
-            validator : function(value){
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        validate: {
+            validator: function (value) {
                 return validator.isEmail(value);
             },
-            message : "Email should be in valid format"
+            message: "Email should be in valid format"
         }
     },
-    password : {
-        type : String,
-        required : function(){
+    password: {
+        type: String,
+        required: function () {
             return !this.googleId;
         },
-        maxlength : [128, "Password cannot exceed 128 charater"],
-        validate : {
-            validator : function(value){
-                if(this.googleId){
+        maxlength: [128, "Password cannot exceed 128 characters"],
+        validate: {
+            validator: function (value) {
+                if (this.googleId) {
                     return true;
                 }
-                
-                return validator.isStrongPassword(value,{
-                    minLength : 8,
-                    minLowercase : 1,
-                    minUppercase : 1,
-                    minNumbers : 1,
-                    minSymbols : 1            
-                })
+                return validator.isStrongPassword(value, {
+                    minLength: 8,
+                    minLowercase: 1,
+                    minUppercase: 1,
+                    minNumbers: 1,
+                    minSymbols: 1
+                });
             },
-            message :  "Password must be stronger"
-        },
-    },
-    phoneNumber : {
-        type : String,
-        required : function(){
-            return !this.googleId;
-        },
-        validate : {
-            validator : function(value){
-                if(this.googleId){
-                    return true;
-                }
-                return validator.isMobilePhone(value , "en-IN")
-            },
-            message : "Phone number should be valid"
+            message: "Password must be stronger"
         }
     },
-    role : {
-        type : String,
-        required : [true,"Role is required"],
-        enum : ["User","Admin"],
-        default : "User"
+    phoneNumber: {
+        type: String,
+        required: function () {
+            return !this.googleId;
+        },
+        validate: {
+            validator: function (value) {
+                if (this.googleId) {
+                    return true;
+                }
+                return validator.isMobilePhone(value, "en-IN");
+            },
+            message: "Phone number should be valid"
+        }
+    },
+    additionalPhoneNumber: {
+        type: String,
+        validate: {
+            validator: function (value) {
+                return validator.isMobilePhone(value, "en-IN");
+            },
+            message: "Additional phone number should be valid"
+        }
+    },
+    role: {
+        type: String,
+        required: [true, "Role is required"],
+        enum: ["User", "Admin"],
+        default: "User"
+    },
+    firstName: {
+        type: String,
+        maxlength: [30, "First name cannot exceed 30 characters"]
+    },
+    lastName: {
+        type: String,
+        maxlength: [30, "Last name cannot exceed 30 characters"]
+    },
+    DOB: {
+        type: Date,
+    },
+    gender: {
+        type: String,
+        enum: ["Male", "Female", "Other"],
+        default: "Other"
+    },
+    profilePicture: {
+        type: String,
+        default: ""
+    },
+    address: {
+        country: { type: String },
+        state: { type: String },
+        city: { type: String },
+        zipCode: { type: String },
+        address1: { type: String }
     }
 });
 
-userSchema.pre("save",async function(next){
-    
+// Hash password before saving
+userSchema.pre("save", async function (next) {
     const user = this;
-    if(!user.isModified("password") || user.googleId) return next();
-    const hashedPassword = await bcrypt.hash(user.password , 10);
-    user.password =  hashedPassword;
+    if (!user.isModified("password") || user.googleId) return next();
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashedPassword;
     next();
-})
+});
 
-module.exports = mongoose.model("User" , userSchema);
+module.exports = mongoose.model("User", userSchema);
+
+
+
+
+
+
+
+
+
+
+
+// const mongoose = require("mongoose");
+// const validator = require("validator");
+// const bcrypt = require("bcrypt");
+
+// const userSchema = mongoose.Schema({
+//     googleId : {
+//         type : String,
+//         default : null
+//     },
+//     name : {
+//         type : String,
+//         required : [true , "Name is required"],
+//         minlength : [3 , "Name should be atleast 3 charater long"],
+//         maxlength : [50, "Name cannot exceed 50 charaters"],
+//         validate :  {
+//             validator : function(value){
+//                 return validator.isAlpha(value , "en-US" , {ignore : " "});
+//             },
+//             message : "Name should be in String"
+//         }
+//     },
+//     email : {
+//         type : String,
+//         required :  [true , "Email is required"],
+//         unique : true,
+//         validate :  {
+//             validator : function(value){
+//                 return validator.isEmail(value);
+//             },
+//             message : "Email should be in valid format"
+//         }
+//     },
+//     password : {
+//         type : String,
+//         required : function(){
+//             return !this.googleId;
+//         },
+//         maxlength : [128, "Password cannot exceed 128 charater"],
+//         validate : {
+//             validator : function(value){
+//                 if(this.googleId){
+//                     return true;
+//                 }
+                
+//                 return validator.isStrongPassword(value,{
+//                     minLength : 8,
+//                     minLowercase : 1,
+//                     minUppercase : 1,
+//                     minNumbers : 1,
+//                     minSymbols : 1            
+//                 })
+//             },
+//             message :  "Password must be stronger"
+//         },
+//     },
+//     phoneNumber : {
+//         type : String,
+//         required : function(){
+//             return !this.googleId;
+//         },
+//         validate : {
+//             validator : function(value){
+//                 if(this.googleId){
+//                     return true;
+//                 }
+//                 return validator.isMobilePhone(value , "en-IN")
+//             },
+//             message : "Phone number should be valid"
+//         }
+//     },
+//     role : {
+//         type : String,
+//         required : [true,"Role is required"],
+//         enum : ["User","Admin"],
+//         default : "User"
+//     }
+// });
+
+// userSchema.pre("save",async function(next){
+    
+//     const user = this;
+//     if(!user.isModified("password") || user.googleId) return next();
+//     const hashedPassword = await bcrypt.hash(user.password , 10);
+//     user.password =  hashedPassword;
+//     next();
+// })
+
+// module.exports = mongoose.model("User" , userSchema);
