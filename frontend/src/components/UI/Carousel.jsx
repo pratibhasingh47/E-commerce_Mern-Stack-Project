49f9e1edd970f2d4b4c3ff19597b7636efa2496d
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-09-30 21:08:24 +0530

    project_updated

--- frontend/src/components/UI/Carousel.jsx
+++ frontend/src/components/UI/Carousel.jsx
@@ -10,50 +10,50 @@ import slider4 from '../../assets/c4.webp';
 
 
 const Carousel = () => {
-  const settings = {
-    dots: true,           // Pagination dots visible
-    infinite: true,       // Infinite loop
-    speed: 500,           // Transition speed in ms
-    slidesToShow: 1,      // Number of slides to show at a time
-    slidesToScroll: 1,    // Number of slides to scroll at a time
-    autoplay: true,       // Auto-play enabled
-    autoplaySpeed: 2000,  // Auto-play speed in ms
-  };
+	const settings = {
+		dots: true,           // Pagination dots visible
+		infinite: true,       // Infinite loop
+		speed: 500,           // Transition speed in ms
+		slidesToShow: 1,      // Number of slides to show at a time
+		slidesToScroll: 1,    // Number of slides to scroll at a time
+		autoplay: true,       // Auto-play enabled
+		autoplaySpeed: 2000,  // Auto-play speed in ms
+	};
 
-  return (
-    <div style={{ width: "90%", margin: "auto", paddingTop: "20px" , marginBottom : '2.5rem' ,  }}>
-      <Slider {...settings}>
-        <div>
-          <img
-            src={slider1}
-            alt="Image 1"
-            style={{ width: "100%", height: "auto" }}
-          />
-        </div>
-        <div>
-          <img
-            src={slider2}
-            alt="Image 2"
-            style={{ width: "100%", height: "auto" }}
-          />
-        </div>
-        <div>
-          <img
-            src={slider3}
-            alt="Image 3"
-            style={{ width: "100%", height: "auto" }}
-          />
-        </div>
-        <div>
-          <img
-            src={slider4}
-            alt="Image 4"
-            style={{ width: "100%", height: "auto" }}
-          />
-        </div>
-      </Slider>
-    </div>
-  );
+	return (
+		<div style={{ width: "90%", margin: "auto", paddingTop: "20px", marginBottom: '2.5rem', }}>
+			<Slider {...settings}>
+				<div>
+					<img
+						src={slider1}
+						alt="Image 1"
+						style={{ width: "100%", height: "auto" }}
+					/>
+				</div>
+				<div>
+					<img
+						src={slider2}
+						alt="Image 2"
+						style={{ width: "100%", height: "auto" }}
+					/>
+				</div>
+				<div>
+					<img
+						src={slider3}
+						alt="Image 3"
+						style={{ width: "100%", height: "auto" }}
+					/>
+				</div>
+				<div>
+					<img
+						src={slider4}
+						alt="Image 4"
+						style={{ width: "100%", height: "auto" }}
+					/>
+				</div>
+			</Slider>
+		</div>
+	);
 };
 
 export default Carousel;
--- frontend/src/components/UI/Cart.jsx
+++ frontend/src/components/UI/Cart.jsx
@@ -6,8 +6,8 @@ const Cart = () => {
     const dispatch = useDispatch();
     const cartItems = useSelector((state) => state.cart.cartItem);
 
-	const theme = useSelector((state) => state.theme?.theme);
-	
+    const theme = useSelector((state) => state.theme?.theme);
+
 
     const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
 
@@ -18,83 +18,83 @@ const Cart = () => {
     };
 
     return (
-		<div className='w-[90%]  mx-auto'>
+        <div className='w-[90%]  mx-auto'>
 
-        <div className="flex flex-wrap justify-center gap-20 p-8">
+            <div className="flex flex-wrap justify-center gap-20 p-8">
 
-            {/* <div className="w-3/6 bg-gray-100 shadow-lg p-10 rounded"> */}
-            <div className={` className="w-2/3 bg-gray-100 shadow-lg p-10 rounded ${theme === 'dark' ? 'bg-slate-800 text-white' : 'bg-gray-100'}`}>
+                {/* <div className="w-3/6 bg-gray-100 shadow-lg p-10 rounded"> */}
+                <div className={` className="w-2/3 bg-gray-100 shadow-lg p-10 rounded ${theme === 'dark' ? 'bg-slate-800 text-white' : 'bg-gray-100'}`}>
 
-                <h1 className="text-3xl font-lato font-bold mb-4">Your Cart</h1>
-                {cartItems.length === 0 ? (
-					<p>Your cart is empty.</p>
-                ) : (
-					<div className="space-y-6">
-                        <ul>
-                            {cartItems.map((item, index) => (
-								<li key={index} className="flex justify-between items-center border-b pb-4 mb-6">
-                                    <div>
-									<img 
-                                            src={`http://localhost:5000/${item.productUrl}`} 
-                                            alt={item.name} 
-                                            className="w-20 h-auto object-cover mb-4 rounded" 
-											/>
-                                        <h2 className="text-xl font-lato font-bold"><strong>Product Name :</strong> {item.name}</h2>
-                                        <p><strong>Price:</strong> {item.price}</p>
-                                        <p><strong>Description:</strong> {item.description}</p>
-                                        <p><strong>Category:</strong> {item.category}</p>
-                                    </div>
-                                    <div className="flex items-center">
-                                        <button
-                                            onClick={() => dispatch(decrementQuantity(item.name))}
-                                            className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-900"
-											>
-                                            -
-                                        </button>
-                                        <span className="px-4">{item.quantity}</span>
-                                        <button
-                                            onClick={() => dispatch(incrementQuantity(item.name))}
-                                            className="bg-green-500 text-white px-3 py-1 rounded hover:bg-green-600"
-											>
-                                            +
-                                        </button>
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
-                        <button 
-                            onClick={handleClearCart} 
-                            className="bg-red-600 font-bold text-white px-4 py-2 rounded hover:bg-red-300 hover:text-black"
-							>
-                            Clear Cart
-                        </button>
-                    </div>
-                )}
+                    <h1 className="text-3xl font-lato font-bold mb-4">Your Cart</h1>
+                    {cartItems.length === 0 ? (
+                        <p>Your cart is empty.</p>
+                    ) : (
+                        <div className="space-y-6">
+                            <ul>
+                                {cartItems.map((item, index) => (
+                                    <li key={index} className="flex justify-between items-center border-b pb-4 mb-6">
+                                        <div>
+                                            <img
+                                                src={`http://localhost:5000/${item.productUrl}`}
+                                                alt={item.name}
+                                                className="w-20 h-auto object-cover mb-4 rounded"
+                                            />
+                                            <h2 className="text-xl font-lato font-bold"><strong>Product Name :</strong> {item.name}</h2>
+                                            <p><strong>Price:</strong> {item.price}</p>
+                                            <p><strong>Description:</strong> {item.description}</p>
+                                            <p><strong>Category:</strong> {item.category}</p>
+                                        </div>
+                                        <div className="flex items-center">
+                                            <button
+                                                onClick={() => dispatch(decrementQuantity(item.name))}
+                                                className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-900"
+                                            >
+                                                -
+                                            </button>
+                                            <span className="px-4">{item.quantity}</span>
+                                            <button
+                                                onClick={() => dispatch(incrementQuantity(item.name))}
+                                                className="bg-green-500 text-white px-3 py-1 rounded hover:bg-green-600"
+                                            >
+                                                +
+                                            </button>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                            <button
+                                onClick={handleClearCart}
+                                className="bg-red-600 font-bold text-white px-4 py-2 rounded hover:bg-red-300 hover:text-black"
+                            >
+                                Clear Cart
+                            </button>
+                        </div>
+                    )}
+                </div>
+
+
+                {/* <div className="w-1/3 h-min bg-gray-100 p-6 rounded-lg shadow-xl"> */}
+                <div className={` w-1/3 h-min bg-gray-100 p-6 rounded-lg shadow-xl ${theme === 'dark' ? 'bg-slate-800 text-white' : 'bg-gray-100'}`}>
+
+                    <h2 className="text-2xl font-lato font-bold mb-4">Cart Summary</h2>
+                    <p className="text-lg">Total Items: {totalItems}</p>
+                    <ul className="mt-4 mb-6">
+                        {cartItems.map((item, index) => (
+                            <li key={index} className="flex justify-between mb-2">
+                                <span>{item.name} ({item.quantity})</span>
+                                <span>{item.price * item.quantity}</span>
+                            </li>
+                        ))}
+                    </ul>
+                    <p className="text-lg font-bold">Total Price: {totalPrice}</p>
+                    <button className="bg-orange-500 text-white font-lato font-bold px-4 py-2 mt-4 w-full  hover:bg-blue-600">
+                        Buy Now
+                    </button>
+                </div>
             </div>
 
 
-            {/* <div className="w-1/3 h-min bg-gray-100 p-6 rounded-lg shadow-xl"> */}
-            <div className={` w-1/3 h-min bg-gray-100 p-6 rounded-lg shadow-xl ${theme === 'dark' ? 'bg-slate-800 text-white' : 'bg-gray-100'}`}>
-
-                <h2 className="text-2xl font-lato font-bold mb-4">Cart Summary</h2>
-                <p className="text-lg">Total Items: {totalItems}</p>
-                <ul className="mt-4 mb-6">
-                    {cartItems.map((item, index) => (
-						<li key={index} className="flex justify-between mb-2">
-                            <span>{item.name} ({item.quantity})</span>
-                            <span>{item.price * item.quantity}</span>
-                        </li>
-                    ))}
-                </ul>
-                <p className="text-lg font-bold">Total Price: {totalPrice}</p>
-                <button className="bg-orange-500 text-white font-lato font-bold px-4 py-2 mt-4 w-full  hover:bg-blue-600">
-                    Buy Now
-                </button>
-            </div>
         </div>
-
-
-		</div>
     );
 };
 
