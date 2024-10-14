a2f8e7d2053c126ce28138153ec22ee4715ed12c
pratibhasingh47 <pratibhasingh1624@gmail.com>
2024-10-14 22:07:10 +0530

    project_updated

--- backend/index.js
+++ backend/index.js
@@ -38,44 +38,44 @@ app.use("/auth", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api", productRoutes);
 app.use("/auth", orderRoutes);
-app.use("/api",cartRoutes);
+app.use("/api", cartRoutes);
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Hello Techno")
 })
 
-app.post("/create-checkout-session",async (req,res)=>{
-    const { products , userId , customerName , customerContactNumber , address , pinCode}  = req.body;
+app.post("/create-checkout-session", async (req, res) => {
+    const { products, userId, customerName, customerContactNumber, address, pinCode } = req.body;
     console.log(req.body)
-    const lineItems = products.map((product)=>(
+    const lineItems = products.map((product) => (
         {
-            price_data : {
-                currency : "inr",
-                product_data : {
-                    name : product.name,
+            price_data: {
+                currency: "inr",
+                product_data: {
+                    name: product.name,
                 },
-                unit_amount : product.price * 100
+                unit_amount: product.price * 100
             },
-            quantity : 1
+            quantity: 1
         }
     ));
 
     const session = await stripe.checkout.sessions.create({
-        payment_method_types : ["card"],
-        line_items : lineItems,
-        mode : "payment",
-        success_url : "http://localhost:5173/paymentsuccess",
-        cancel_url : "http://localhost:5173/cancelPayment"
+        payment_method_types: ["card"],
+        line_items: lineItems,
+        mode: "payment",
+        success_url: "http://localhost:5173/paymentsuccess",
+        cancel_url: "http://localhost:5173/cancelPayment"
     });
 
     const order = new Order({
-        product : products , userId , customerName , customerContactNumber , address , pinCode : +pinCode , transactionId : session.id
+        product: products, userId, customerName, customerContactNumber, address, pinCode: +pinCode, transactionId: session.id
     });
 
     await order.save();
 
-    res.json({id : session.id});
+    res.json({ id: session.id });
 
 })
 
